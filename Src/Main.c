040042902a7a2c7d75b5b7dc5ee7faffb3881613
Yatwohee <jisanduo@outlook.com>
2025-07-06 17:57:42 +0800

    Fake source code

---
+++ Src/Main.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: xixi_
+ * @Date: 2025-07-06 17:56:35
+ * @LastEditors: xixi_
+ * @LastEditTime: 2025-07-06 17:57:05
+ * @FilePath: /Xncut/Src/Main.cpp
+ * Copyright (c) 2020-2025 by xixi_ , All Rights Reserved.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("假的嘿嘿\n");
+}
