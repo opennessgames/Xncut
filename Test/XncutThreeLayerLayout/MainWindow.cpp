509de5d5a224911006ed2e551ac19f2cb29b8d71
xixi_ <yyx@Linux114514.xixi.org>
2025-10-30 23:09:57 +0800

    三层垂直布局部件

--- Test/XncutThreeLayerLayout/MainWindow.cpp
+++ Test/XncutThreeLayerLayout/MainWindow.cpp
@@ -2,7 +2,7 @@
  * @Author: xixi_
  * @Date: 2025-10-25 13:20:25
  * @LastEditors: xixi_
- * @LastEditTime: 2025-10-25 13:20:38
+ * @LastEditTime: 2025-10-30 23:08:46
  * @FilePath: /XncutUI/Test/XncutThreeLayerLayout/MainWindow.cpp
  * Copyright (c) 2020-2025 by xixi_ , All Rights Reserved.
  */
@@ -13,13 +13,25 @@
 #include <QToolBar>
 #include <QPushButton>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+MainWindow::MainWindow(QWidget *Parent)
+    : QMainWindow(Parent)
 {
+    /* 初始化 */
+    /* 部件 */
     XncutThreeLayerLayoutWidget *ThreeLayerLayoutWidget = new XncutThreeLayerLayoutWidget(new QWidget());
+    /********************************************************************************************************/
+
+    /* 设置属性 */
+    /* 部件 */
     ThreeLayerLayoutWidget->GetTopTitleLabel()->setText("这是一个标题");
     ThreeLayerLayoutWidget->GetBottomToolBar()->addWidget(new QPushButton());
+    /********************************************************************************************************/
+
+    /* 设置中心部件 */
     setCentralWidget(ThreeLayerLayoutWidget);
+    /********************************************************************************************************/
 }
 
-MainWindow::~MainWindow() {}
+MainWindow::~MainWindow()
+{
+}
