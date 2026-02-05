a4d514b874454d811ff2774a7bd69c22bac6dce9
opennessgames <jisanduo@outlook.com>
2026-02-06 00:08:54 +0800

    添加应用程序图标和欢迎界面

--- Xncut/XncutMain.cpp
+++ Xncut/XncutMain.cpp
@@ -2,23 +2,39 @@
  * @Author: xixi_
  * @Date: 2026-02-05 23:45:14
  * @LastEditors: xixi_
- * @LastEditTime: 2026-02-05 23:50:19
+ * @LastEditTime: 2026-02-06 00:07:52
  * @FilePath: /Xncut/Xncut/XncutMain.cpp
  * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
  */
 
 #include <QApplication>
 #include "Xncut.h"
+#include "XncutWelcome/XncutWelcomeScreen.h"
 
 int main(int argc, char **argv)
 {
     QApplication OpenGameApplication(argc, argv);
-    OpenGameApplication.setApplicationName("Xncut");
-    OpenGameApplication.setApplicationDisplayName("openGAME熙柠剪辑");
     /********************************************************************************************************/
 
+    OpenGameApplication.setApplicationName("Xncut");
+    OpenGameApplication.setApplicationDisplayName("openGAME熙柠剪辑");
+    OpenGameApplication.setOverrideCursor(Qt::WaitCursor); /* 等待光标 */
+    /********************************************************************************************************/
+
+    /* 初始化一些物品 */
+    XncutWelcomeScreen WelcomeScreen(NULL);
     Xncut XncutApplication(NULL);
+    /********************************************************************************************************/
+
+    /* 欢迎界面 */
+    WelcomeScreen.setFixedSize(QSize(900, 444));
+    WelcomeScreen.setPixmap(QPixmap("://Images/Welcome/StartBg.png").scaled(WelcomeScreen.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    WelcomeScreen.show();
+
+    /* 主窗口 */
     XncutApplication.setWindowTitle("剪辑大厅");
+    WelcomeScreen.finish(&WelcomeScreen);
+    OpenGameApplication.restoreOverrideCursor(); /* 光标恢复正常 */
     XncutApplication.show();
     /********************************************************************************************************/
 
---
+++ Xncut/XncutWelcome/XncutWelcomeScreen.cpp
@@ -0,0 +1,31 @@
+/*
+ * @Author: xixi_
+ * @Date: 2026-02-05 23:58:11
+ * @LastEditors: xixi_
+ * @LastEditTime: 2026-02-05 23:58:16
+ * @FilePath: /Xncut/Xncut/XncutWelcome/XncutWelcomeScreen.cpp
+ * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
+ */
+
+#include "XncutWelcomeScreen.h"
+#include <QMouseEvent>
+
+XncutWelcomeScreen::XncutWelcomeScreen(QObject *Parent)
+{
+    setMouseTracking(false);
+}
+
+void XncutWelcomeScreen::mousePressEvent(QMouseEvent *Event)
+{
+    /**
+     * 忽略掉
+     * 如果不忽略,鼠标点击欢迎屏幕时,将会消失
+     **/
+    Event->ignore();
+}
+
+/* 鼠标移动事件 */
+void XncutWelcomeScreen::mouseMoveEvent(QMouseEvent *Event)
+{
+    Event->ignore();
+}
