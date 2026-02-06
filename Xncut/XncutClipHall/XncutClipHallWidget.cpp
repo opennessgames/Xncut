7e5d83ffb9d46aafc6156b09e00f8ca11cf7ebc2
opennessgames <jisanduo@outlook.com>
2026-02-06 16:10:59 +0800

    搭建剪辑大厅框架

---
+++ Xncut/XncutClipHall/XncutClipHallWidget.cpp
@@ -0,0 +1,19 @@
+/*
+ * @Author: xixi_
+ * @Date: 2026-02-06 16:06:41
+ * @LastEditors: xixi_
+ * @LastEditTime: 2026-02-06 16:10:07
+ * @FilePath: /Xncut/Xncut/XncutClipHall/XncutClipHallWidget.cpp
+ * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
+ */
+
+#include "XncutClipHallWidget.h"
+
+XncutClipHallWidget::XncutClipHallWidget(QWidget *Parent)
+    : QWidget(Parent)
+{
+}
+
+XncutClipHallWidget::~XncutClipHallWidget()
+{
+}
