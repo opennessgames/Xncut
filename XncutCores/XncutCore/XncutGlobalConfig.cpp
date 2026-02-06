db7612a32c34038405c5d52dc18addc23d78f131
opennessgames <jisanduo@outlook.com>
2026-02-06 14:31:59 +0800

    新增XncutCores模块

---
+++ XncutCores/XncutCore/XncutGlobalConfig.cpp
@@ -0,0 +1,8 @@
+/*
+ * @Author: xixi_
+ * @Date: 2026-02-06 14:28:16
+ * @LastEditors: xixi_
+ * @LastEditTime: 2026-02-06 14:28:41
+ * @FilePath: /Xncut/XncutCores/XncutCore/XncutGlobalConfig.cpp
+ * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
+ */
---
+++ XncutCores/XncutCore/XncutGlobalConfig.h
@@ -0,0 +1,8 @@
+/*
+ * @Author: xixi_
+ * @Date: 2026-02-06 14:28:16
+ * @LastEditors: xixi_
+ * @LastEditTime: 2026-02-06 14:28:43
+ * @FilePath: /Xncut/XncutCores/XncutCore/XncutGlobalConfig.h
+ * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
+ */
