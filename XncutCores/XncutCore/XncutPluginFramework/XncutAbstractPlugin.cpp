4ac4cddc595883c9a5a2f16dac2c6937776ee376
opennessgames <jisanduo@outlook.com>
2026-02-06 14:48:08 +0800

    搭建抽象插件框架

---
+++ XncutCores/XncutCore/XncutPluginFramework/XncutAbstractPlugin.cpp
@@ -0,0 +1,10 @@
+/*
+ * @Author: xixi_
+ * @Date: 2026-02-06 14:46:24
+ * @LastEditors: xixi_
+ * @LastEditTime: 2026-02-06 14:46:40
+ * @FilePath: /Xncut/XncutCores/XncutCore/XncutPluginFramework/XncutAbstractPlugin.cpp
+ * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
+ */
+
+#include "XncutAbstractPlugin.h"
