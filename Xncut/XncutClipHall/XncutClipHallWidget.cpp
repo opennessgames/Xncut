76fca7964fa6fdeef0203076fc86e00e94407459
opennessgames <jisanduo@outlook.com>
2026-02-19 15:13:20 +0800

    希望!基本的框架

--- Xncut/XncutClipHall/XncutClipHallWidget.cpp
+++ Xncut/XncutClipHall/XncutClipHallWidget.cpp
@@ -2,16 +2,32 @@
  * @Author: xixi_
  * @Date: 2026-02-06 16:06:41
  * @LastEditors: xixi_
- * @LastEditTime: 2026-02-06 16:10:07
+ * @LastEditTime: 2026-02-19 15:11:39
  * @FilePath: /Xncut/Xncut/XncutClipHall/XncutClipHallWidget.cpp
  * Copyright (c) 2020-2026 by xixi_ , All Rights Reserved.
  */
 
 #include "XncutClipHallWidget.h"
+#include <QGridLayout>
 
 XncutClipHallWidget::XncutClipHallWidget(QWidget *Parent)
     : QWidget(Parent)
 {
+    /* 初始化 */
+    QGridLayout *MainLayout = new QGridLayout();
+    /********************************************************************************************************/
+
+    /* 设置属性 */
+    /********************************************************************************************************/
+
+    /* 设置布局 */
+    /********************************************************************************************************/
+
+    setLayout(MainLayout);
+    /********************************************************************************************************/
+
+    /* 连接信号槽 */
+    /********************************************************************************************************/
 }
 
 XncutClipHallWidget::~XncutClipHallWidget()
